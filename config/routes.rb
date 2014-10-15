781098c7ecc22fa5c0e448fe0a45d6a89b73b048
lipin <ping.li1688@gmail.com>
2014-10-15 10:32:32 +0800

    sunny - style && routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-
+  get 'about'=>'home#about'
+  get 'life'=>'home#life'
+  get 'share'=>'home#share'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
