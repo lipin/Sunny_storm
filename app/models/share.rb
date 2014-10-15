8e812b355e698873672c8547f99430f8b4c3f989
lipin <ping.li1688@gmail.com>
2014-10-15 22:33:49 +0800

    sunny

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: shares
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Share < ActiveRecord::Base
 end
