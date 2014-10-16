4e2e318974d7e52dc262e97d552bde7bb4e72f29
lipin <ping.li1688@gmail.com>
2014-10-16 11:56:38 +0800

    sunny

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,21 @@ AdminUser.create(
   password: 'password'
 )
 puts "crate example user success"
+
+Article.create(
+	title: '现实社会',
+	content: '这是个现实的社会，感情不能当饭吃，贫穷夫妻百事哀。不要相信电影里的故事情节，那只是个供许多陌生人喧嚣情感的场所。只有不理智和不现实的人才相信'
+	)
+Article.create(
+	title: '算卦星座命理',
+	content: '不要相信算卦星座命理，那是哄小朋友的，命运掌握在自己手中。坐在家里等什么房子，车子,还不如睡一觉做个好梦。'
+	)
+Article.create(
+	title: '出路出路',
+	content: '出路出路，走出去了，总是会有路的。困难苦难，困在家里就是难。《社会调查》普遍认为。'
+	)
+Article.create(
+	title: '人都有惰性',
+	content: ' 是人都有惰性，这是与生俱来的，但是我们后天可以改变这种惰性，因为有很多人正在改变。对于某种事物或是生意不要等别人做到了，我才想到。不要等别人已经赚到钱了，我才想去做。没有人相信的是市场和机遇，大家都相信的叫做膨胀。'
+	)
+puts 'creating articles success..'
